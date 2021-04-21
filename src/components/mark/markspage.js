608a4b5e0d3a8e8581f03c6847400560384ae422
Mark Leung <lhhmark@gmail.com>
2021-04-21 15:57:39 +0800

    update the structure of MarksPage

---
+++ src/components/mark/markspage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import sectionOne from './sectionOne';
+import sectionTwo from './sectionTwo';
+
+
+const MarksPage = () => {
+	<>
+		<sectionOne />
+		<sectionTwo />
+	</>
+};
+
+export default MarksPage;
